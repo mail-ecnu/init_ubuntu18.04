03cf453e09fe22709ac08dcf9eb80538d9f4af88
cww97 <cww970329@qq.com>
2021-01-03 22:10:31 +0800

    autojump & timezone

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,4 +1,9 @@
 sudo apt install zsh -y 
 
-sh -c "$(wget -O- https://gitee.com/pocmon/ohmyzsh/raw/master/tools/install.sh)" 
+sh -c "$(wget -O- https://gitee.com/pocmon/ohmyzsh/raw/master/tools/install.sh)"
 
+
+# follow here to install autojump
+# https://gitee.com/mirror-github/autojump
+# dont forget to add sth into .zshrc
+#
--- main.sh
+++ main.sh
@@ -6,6 +6,12 @@ sudo cp sources.list /etc/apt/sources.list
 sudo apt update
 sudo apt upgrade -y
 
+# time zone 
+# sudo cp /usr/share/zoneinfo/Asia/Shanghai  /etc/localtime
+# csl: ln is better
+sudo ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+echo 'Asia/Shanghai' >/etc/timezone
+
 # sth u always need
 sudo apt install wget curl git g++ openssh-server htop tmux -y
 
